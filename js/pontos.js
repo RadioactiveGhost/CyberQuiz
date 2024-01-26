8e9d6e6602ab7834bb31aceabd73c1fb10f5f95c
RafaDarque <miurnin97@gmail.com>
2024-01-26 12:06:11 +0000

    cenas

--- js/pontos.js
+++ js/pontos.js
@@ -9,19 +9,24 @@ function processarPontos() {
     let preencher = document.getElementById("preencher");
     preencher.innerHTML = 'TESTE'
     if (pontosLocal < 2) {
-        preencher.innerHTML = "Muito Mau!";
+        preencher.innerHTML = "Muito Mau!<br>";
+        preencher.innerHTML += "<img class=\"emoji\" width=60 src=\"../assets/img/muitomau.png\">";
     }
     else if (pontosLocal < 4) {
         preencher.innerHTML = "Mau!";
+        preencher.innerHTML += "<img class=\"emoji\" width=60 src=\"../assets/img/mau.png\">";
     }
     else if (pontosLocal < 6) {
         preencher.innerHTML = "Bom!";
+        preencher.innerHTML += "<img class=\"emoji\" width=60 src=\"../assets/img/bom.png\">";
     }
     else if (pontosLocal < 9) {
         preencher.innerHTML = "Muito Bom!";
+        preencher.innerHTML += "<img class=\"emoji\" width=60 src=\"../assets/img/muitobom.png\">";
     }
     else {
         preencher.innerHTML = "Excelente!";
+        preencher.innerHTML += "<img class=\"emoji\" width=60 src=\"../assets/img/excelente.png\">";
     }
     preencher.innerHTML+= "\
     <br><br>Acertaste "+ pontosLocal +" de "+ totalLocal +" desafios\
