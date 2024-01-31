3f79658296fe0746585171d7165ec776993bda96
RadioactiveGhost <radioactiveghost96@gmail.com>
2024-01-31 17:59:14 +0000

    Pequenos ajustes

--- dashboard.php
+++ dashboard.php
@@ -55,17 +55,17 @@
                 } else {
                     echo "Tempo a mais";
                 }
-                echo "</td><td>" . $row["SUGESTAO"] . "</td><td>" . $row["RESULTADO_FINAL"] . "</td>";
+                echo "</td><td>" . $row["SUGESTAO"] . "</td><td class='center'>" . $row["RESULTADO_FINAL"] . "</td>";
                 $sql2 = "SELECT  	ID_UTILIZADOR_NIVEL, VALOR_NIVEL, ID_UTILIZADOR FROM utilizadores_niveis WHERE ID_UTILIZADOR=" . $row["ID_UTILIZADOR"];
                 $result2 = $conn->query($sql2);
                 if ($result2->num_rows > 0) {
                     while($row2 = $result2->fetch_assoc()) {
                         if ($row2["VALOR_NIVEL"] == 1) {
-                            echo "<td class='points'><i class='fa-solid fa-check'></i></td>";
+                            echo "<td class='center'><i class='fa-solid fa-check'></i></td>";
                         } else if ($row2["VALOR_NIVEL"] == 0) {
-                            echo "<td class='points'><i class='fa-solid fa-xmark'></i></td>";
+                            echo "<td class='center'><i class='fa-solid fa-xmark'></i></td>";
                         } else {
-                            echo "<td class='points'>N/A</td>";
+                            echo "<td class='center'>N/A</td>";
                         }
                         //echo "<td>" . $row2["VALOR_NIVEL"] . "</td>";
                     }
